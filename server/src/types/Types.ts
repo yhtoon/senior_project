88bf074fa9fab39799cc70baf6416e57f7d68bb3
amnehalsuqi <aalsu013@ucr.edu>
2022-11-26 11:23:27 -0800

    types for cand

--- server/src/types/Types.ts
+++ server/src/types/Types.ts
@@ -7,6 +7,9 @@ export type office = string;
 export type party = string;
 export type state = string;
 
+export type ici = string; 
+export type year = string;
+
 // object attributes
 export interface commieInfoItem {
   CAND_PTY_AFFILIATION: party,
@@ -23,4 +26,28 @@ export interface commieInfoItem {
 export interface CommieInfoResponse {
   COMM_ID: id,
   committeeTable: commieInfoItem[],
+}
+
+export interface CAND_INFO {
+  CAND_ID: id,
+  CAND_NAME: name,
+  CAND_ICI: ici,
+  CAND_OFFICE_ST: state,
+  CAND_PCC: id, 
+  CAND_OFFICE: office,
+  CAND_OFFICE_DISTRICT: district, 
+  CAND_ELECTION_YR: year, 
+}
+
+export interface exploreResp {
+  CID: id
+  info: {
+      [key: string]: CAND_INFO
+  };
+}
+
+export interface doraResp {
+  info: {
+    [key: string]: CAND_INFO
+};
 }
