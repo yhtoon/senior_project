d80d5c6f49ab7496baf503aaec5212845108ecf0
aungthuhein13 <henryaungthuhein@gmail.com>
2022-11-29 18:17:20 -0800

    Change and add hashes files

---
+++ client/src/components/constant/hashes_2.ts
@@ -0,0 +1,42 @@
+export const OfficeMap = new Map<string, string>([['House', 'H'], ['President', 'P'], ['Senate', 'S']]);
+
+export const StateMap = new Map<string, string>([[ 'Alabama', 'AL'], ['Alaska', 'AK'], ['Arizona', 'AZ'], ['Arkansas', 'AR'], ['American Samoa', 'AS'],
+        ['California', 'CA'], ['Colorado', 'CO'], ['Connecticut', 'CT'], ['Delaware', 'DE'], ['District of Columbia', 'DC'],
+        ['FL', 'Florida'], ['GA', 'Georgia'], ['GU', 'Guam'], ['HI', 'Hawaii'], ['ID', 'Idaho'],
+        ['Illinois', 'IL'], ['Indiana', 'IN'], ['Iowa', 'IA'], ['Kansas', 'KS'], ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'], ['Maine', 'ME'], ['Maryland', 'MD'], ['Massachusetts', 'MA'], ['Michigan', 'MI'],
+        ['Minnesota', 'MN'], ['Mississippi', 'MS'], ['Missouri', 'MO'], ['Montana', 'MT'], ['Nebraska', 'NE'],
+        ['Nevada', 'NV'], ['New Hampshire', 'NH'], ['New Jersey', 'NJ'], ['New Mexico', 'NM'], ['New York', 'NY'],
+        ['North Carolina', 'NC'], ['North Dakota', 'ND'], ['Northern Mariana Islands', 'MP'], ['Ohio', 'OH'], ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'], ['Pennsylvania', 'PA'], ['Puerto Rico', 'PR'], ['Rhode Island', 'RI'], ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'], ['Tennessee', 'TN'], ['Texas', 'TX'], ['Utah', 'UT'], ['Vermont', 'VT'],
+        ['Virginia', 'VA'], ['Virgin Islands', 'VI'], ['Washington', 'WA'], ['West Virginia', 'WV'], ['Wisconsin', 'WI'],
+        ['Wyoming', 'WT']]);
+
+export const EntityMap = new Map<string, string>([['Candidate', 'CAN'], ['Candidate Committee Master', 'CCM'], ['Committee', 'COM'],
+        ['Individual', 'IND'], ['Organization', 'ORG'], ['Political Action Committee', 'PAC'], ['Party Organization', 'PTY']]);
+
+export const CandIciMap = new Map<string, string>([['Challenger', 'C'], ['Incumbent', 'I'], ['Open Seat', 'O']]);
+
+export const PartyMap = new Map<string, string>([['Ace Party', 'ACE'], ['Alaskan Independence Party', 'AKI'], ['American Independence Conservative', 'AIC'],
+        ['American Independence Party', 'AIP'], ['American Party', 'AMP'], ["American People's Freedom Party",  "APF"],
+        ['Americans Elect', 'AE'], ["Citizens' Party",  "CIT"], ['Commandments Party', 'CMD'],
+        ['Commonwealth Party of the U.S.', 'CMP'], ['Communist Party', 'COM'], ['Concerned Citizens Party Of Connecticut', 'CNC'],
+        ['Conservative Party', 'CRV'], ['Constitution Party', 'CON'], ['Constitutional', 'CST'], ['Country', 'COU'],
+        ['D.C. Statehood Green Party', 'DCG'], ['Democratic -Nonpartisan League', 'DNL'], ['Democratic Party', 'DEM'], ['Democratic/Conservative', 'D/C'],
+        ['Democratic-Farmer-Labor', 'DFL'], ['Desert Green Party', 'DGR'], ['Federalist', 'Fed'], ['Freedom Labor Party', 'FLP'], ['Freedom Party', 'FRE'],
+        ['George Wallace Party', 'GWP'], ['Grassroots', 'GRT'], ['Green Party', 'GRE'], ['Green-Rainbow', 'GR'], ['Human Rights Party', 'HRP'],
+        ['Independence Party', 'IDP'], ['Independent', 'IND'], ['Independent American Party', 'IAP'], ['Independent Conservative Democratic', 'ICD'],
+        ['Independent Conservative Democratic', 'IGR'], ['Independent Party', 'IP'], ['Independent Party of Delaware', 'IDE'], ['Industrial Government Party', 'IGD'],
+        ['Justice Party', 'JCN'], ['Justice Party', 'JUS'], ['La Raza Unida', 'LRU'], ['Labor Party', 'LBR'], ['Less Federal Taxes', 'LFT'],
+        ['Liberal Party', 'LBL'], ['Libertarian Party', 'LIB'], ['Libertarian Party', 'LBU'], ['Libertarian Party', 'MTP'], ['National Democratic Party', 'NDP'],
+        ['National Democratic Party', 'NLP'], ['New Alliance', 'NA'], ['New Jersey Conservative Party', 'NJC'], ['New Progressive Party', 'NPP'], ['No Party Affiliation', 'NPA'],
+        ['No Party Preference', 'NOP'], ['None', 'NNE'], ['Nonpartisan', 'N'], ['Non-Party', 'NON'], ['One Earth Party', 'OE'], ['Other', 'OTH'], ['Pacific Green', 'PG'],
+        ['Party for Socialism and Liberation', 'PSL'], ['Peace And Freedom', 'PAF'], ['Peace And Freedom Party', 'PFP'], ['Peace Freedom Party', 'PFD'],
+        ['People Over Politics', 'POP'], ["People's Party", "PPY"], ['Personal Choice Party', 'PCH'], ['Popular Democratic Party', 'PPD'], ['Progressive Party', 'PRO'],
+        ['Prohibition Party', 'NAP'], ['Puerto Rican Independence Party', 'PRI'], ['Raza Unida Party', 'RUP'], ['Reform Party', 'REF'], ['Republican Party', 'REP'],
+        ['Resource Party', 'RES'], ['Right To Life', 'RTL'], ['Socialist Equality Party', 'SEP'], ['Socialist Labor Party', 'SLP'], ['Socialist Party', 'SUS'],
+        ['Socialist Party U.S.A.', 'SOC'], ['Socialist Workers Party', 'SWP'], ['Taxpayers', 'TX'], ['Taxpayers Without Representation', 'TWR'],
+        ['Tea Party', 'TEA'], ['Theo-Democratic', 'THD'], ['U.S. Labor Party', 'LAB'], ["U.S. People's Party", "USP"], ['U.S. Taxpayers Party', 'UST'],
+        ['Unaffiliated', 'UN'], ['United Citizen', 'UC'], ['United Party', 'UNI'], ['Unknown', 'UNK'], ['Veterans Party', 'VET'], ['We the People', 'WTP'],
+        ['Write-In', 'W']]);
